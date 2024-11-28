d1cecdbe24522da8e4d13bcc47bf7e6f0deb60b5
Eva <ekoss.prog@gmail.com>
2024-11-28 17:28:48 +0400

    Initial setup: Configure React, Next.js, and Tailwind CSS environment

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>Hi, Eva! You're doing so great! It's your first personal page on Next/Tailwind/React :3</h1>
+  )
+}
+
+export default page

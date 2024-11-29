f158be0c1148089d6dcdb33e1e598abf6aa12b1d
Eva <ekoss.prog@gmail.com>
2024-11-29 12:23:32 +0400

    Set up basic Tailwind CSS configuration

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <h1>Hi, Eva! You're doing so great! It's your first personal page on Next/Tailwind/React :3</h1>
-  )
-}
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        laudantium quaerat distinctio iusto at aliquam tempore, quidem facere
+        veritatis! Officia quasi itaque reiciendis dolorem mollitia,
+        praesentium, numquam magni qui et nesciunt, nisi nemo saepe id cum.
+        Quisquam incidunt, asperiores officiis tempore commodi animi cum ex
+        labore impedit debitis pariatur ipsam?
+      </p>
+    </div>
+  );
+};
 
-export default page
+export default page;

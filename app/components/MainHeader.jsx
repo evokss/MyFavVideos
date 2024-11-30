10a4b23725ca84bab86b3a4d228f7f2f5f6724ed
Eva <ekoss.prog@gmail.com>
2024-11-30 19:02:22 +0400

    Implement aside section and enhance header styling

--- app/components/MainHeader.jsx
+++ app/components/MainHeader.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import { FcFilmReel } from "react-icons/fc";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import { CiSearch } from "react-icons/ci";
 
 const MainHeader = () => {
   return (
-    <div className="bg-white flex justify-between items-center gap-x-4 pr-4 pl-4 h-14">
-      <div className="basis-1/4 flex items-center">
-        <div className="p-2">
+    <div className="bg-white flex justify-between items-center p-4 h-14 mb-4">
+      <div className="flex items-center basis-1/4">
+        <div className="flex p-2 pr-0">
           <HiOutlineBars3 className="size-6 cursor-pointer" />
+          <FcFilmReel className="size-6 ml-4" />
         </div>
         <h1>MyFavVideos</h1>
       </div>
-      <div className="basis-2/4 w-full flex flex-row items-center">
-        <div className="border border-zinc-300 rounded-l-full pl-4 h-10 flex items-center">
+      <div className="hidden sm:flex flex-row items-center basis-1/2">
+        <div className="border border-zinc-300 rounded-l-full pl-4 h-10 flex items-center flex-1">
           <input className="border-0 focus:outline-none" placeholder="Search" />
         </div>
         <button className="border border-zinc-300 rounded-r-full border-l-0 h-10">
@@ -22,14 +24,14 @@ const MainHeader = () => {
           </div>
         </button>
       </div>
-      <div className="">
+      <div className="basis-1/6 justify-items-end">
         <Image
-          className="rounded-2xl"
+          className="rounded-3xl"
           src="/images/profile-photo.webp"
           alt="A user profile picture"
-          quality={90}
-          width={32}
-          height={32}
+          quality={95}
+          width={35}
+          height={35}
         />
       </div>
     </div>
--- app/components/MainLayout.jsx
+++ app/components/MainLayout.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import MainHeader from "./MainHeader";
+import Link from "next/link";
+
+import { IoHomeOutline } from "react-icons/io5";
+import { AiOutlineCompass } from "react-icons/ai";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { BsFileEarmarkPlay } from "react-icons/bs";
+import { RiPlayListLine } from "react-icons/ri";
+import { IoBulbOutline } from "react-icons/io5";
 
 const MainLayout = ({ children }) => {
   return (
-    <div>
+    <div className="bg-gray-100 w-screen min-h-screen">
       <MainHeader />
-      <h1>Main Layout</h1>
-      <hr />
-      <main>{children}</main>
+      <div className="flex justify-start items-start">
+        <aside className="bg-white rounded-r-lg w-60 p-4">
+          <ul>
+            <li className="flex justify-start items-center hover:bg-blue-100 hover:text-blue-800 rounded-lg p-2 cursor-pointer">
+              <IoHomeOutline className="mr-2" />
+              <Link href="/">Home</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-orange-100 hover:text-orange-800 rounded-lg p-2 cursor-pointer">
+              <AiOutlineCompass className="mr-2" />
+              <Link href="/">Explore</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-orange-100 hover:text-orange-800 rounded-lg p-2 cursor-pointer">
+              <BsFileEarmarkPlay className="mr-2" />
+              <Link href="/">Shorts</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-orange-100 hover:text-orange-800 rounded-lg p-2 cursor-pointer">
+              <RiPlayListLine className="mr-2" />
+              <Link href="/">Playlists</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-orange-100 hover:text-orange-800 rounded-lg p-2 cursor-pointer">
+              <MdOutlineSubscriptions className="mr-2" />
+              <Link href="/">Subscriptions</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-pink-100 hover:text-pink-800 rounded-lg p-2 cursor-pointer">
+              <IoBulbOutline className="mr-2" />
+              <Link href="/">About me</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="flex-1">{children}</main>
+      </div>
     </div>
   );
 };

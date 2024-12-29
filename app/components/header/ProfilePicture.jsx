8b79ef6cb5277e16cd092196e9706acc25bf312f
Eva <ekoss.prog@gmail.com>
2024-12-29 15:21:58 +0400

    Enhance ProfilePicture component with clickable link to portfolio

--- app/components/header/ProfilePicture.jsx
+++ app/components/header/ProfilePicture.jsx
@@ -3,15 +3,22 @@ import Image from "next/image";
 
 const ProfilePicture = () => {
   return (
-    <div className="basis-1/6 justify-items-end p-2">
-      <Image
-        className="rounded-3xl"
-        src="/profile-pictures/profile-photo.webp"
-        alt="A user profile picture"
-        quality={95}
-        width={35}
-        height={35}
-      />
+    <div className="basis-1/6 p-2 flex justify-end">
+      <a
+        href="https://portfolio-website-evokss.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block"
+      >
+        <Image
+          className="rounded-3xl cursor-pointer"
+          src="/profile-pictures/profile-photo.webp"
+          alt="A user profile picture"
+          quality={95}
+          width={35}
+          height={35}
+        />
+      </a>
     </div>
   );
 };

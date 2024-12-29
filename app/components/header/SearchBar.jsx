315fe4c6fdf28ccfa58c3ff7b8d413563a7ebf47
Eva <ekoss.prog@gmail.com>
2024-12-29 15:22:43 +0400

    Improve SearchBar component input alignment and button behavior

--- app/components/header/SearchBar.jsx
+++ app/components/header/SearchBar.jsx
@@ -4,12 +4,15 @@ import { CiSearch } from "react-icons/ci";
 const SearchBar = () => {
   return (
     <div className="hidden sm:flex flex-row items-center basis-1/2">
-      <div className="border border-zinc-300 rounded-l-full pl-4 h-10 flex items-center flex-1">
-        <input className="border-0 focus:outline-none" placeholder="Search" />
+      <div className="border border-zinc-300 rounded-l-full h-10 flex items-center flex-1">
+        <input
+          className="flex-grow border-0 focus:outline-none ml-4"
+          placeholder="Search"
+        />
       </div>
-      <button className="border border-zinc-300 rounded-r-full border-l-0 h-10">
+      <button className="border border-zinc-300 rounded-r-full border-l-0 h-10 cursor-default">
         <div className="py-2 pl-3 pr-5">
-          <CiSearch className="size-6 cursor-pointer" />
+          <CiSearch className="size-6" />
         </div>
       </button>
     </div>

bd7cc178a7ba943727d430fda956d225977770da
Eva <ekoss.prog@gmail.com>
2024-12-29 15:24:02 +0400

    Enhance SidebarItem component for external links and cursor styling

--- app/components/sidebar/SidebarItem.jsx
+++ app/components/sidebar/SidebarItem.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Link from "next/link";
 
-const SidebarItem = ({ icon, label, href, hoverColor, margin }) => {
+const SidebarItem = ({ icon, label, href, hoverColor, margin, cursor }) => {
   const hoverClasses = {
-    blue: "hover:bg-blue-100 hover:text-blue-800",
-    orange: "hover:bg-orange-100 hover:text-orange-800",
+    gray: "hover:bg-gray-100 hover:text-gray-600",
     pink: "hover:bg-pink-100 hover:text-pink-800",
   };
+  const isLinkExternal = href.startsWith("https");
 
   return (
     <li
-      className={`flex justify-start items-center ${hoverClasses[hoverColor]} rounded-lg p-2 cursor-pointer`}
+      className={`flex justify-start items-center ${hoverClasses[hoverColor]} rounded-lg p-2`}
     >
       {icon}
-      <Link href={href} className={margin}>
-        {label}
-      </Link>
+      {isLinkExternal ? (
+        <a
+          href={href}
+          className={`${margin} ${cursor}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {label}
+        </a>
+      ) : (
+        <Link href={href} className={`${margin} ${cursor}`}>
+          {label}
+        </Link>
+      )}
     </li>
   );
 };

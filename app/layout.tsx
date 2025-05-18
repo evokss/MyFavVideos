d00cb02de2985c4a1dc49936d6dd6dc68615d37d
Eva <ekoss.prog@gmail.com>
2025-05-18 13:15:39 +0400

    feat(root-layout): add TypeScript support and improve metadata description
    - Add RootLayoutProps type for children prop in layout.tsx
    - Update metadata description to be more user-friendly and include TypeScript
    - Rename layout.js to layout.tsx for JSX and TypeScript compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,17 @@ import MenuProvider from "@/context/MenuContext";
 export const metadata = {
   title: "My Favorite Videos",
   description:
-    "This is React/Next/Tailwind project for displaying Eva's favorite YouTube videos",
+    "Check out a handpicked collection of YouTube videos in this clean, easy-to-use app built with TypeScript, React, Next.js, and Tailwind CSS.",
   icons: {
     icon: "/favicon.png",
   },
 };
 
-export default function RootLayout({ children }) {
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>

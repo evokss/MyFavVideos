aa974fb530671f58616744f2a8a1df1173f27aa9
Eva <ekoss.prog@gmail.com>
2025-05-25 13:07:00 +0400

    refactor(SearchBar): convert component to TSX with type annotations
    - Updated SearchBar component from JSX to TSX
    - Added React.FC type for explicit component typing
    - Included type="text" for input and type="button" for button to improve semantics
    - Ensured compatibility with TypeScript by maintaining existing functionality

--- app/components/header/SearchBar.tsx
+++ app/components/header/SearchBar.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 
-const SearchBar = () => {
+const SearchBar: React.FC = () => {
   return (
     <div className="hidden sm:flex flex-row items-center basis-1/2">
       <div className="border border-zinc-300 rounded-l-full h-10 flex items-center flex-1">
         <input
           className="flex-grow border-0 focus:outline-none ml-4"
           placeholder="Search"
+          type="text"
         />
       </div>
-      <button className="border border-zinc-300 rounded-r-full border-l-0 h-10 cursor-default">
+      <button
+        className="border border-zinc-300 rounded-r-full border-l-0 h-10 cursor-default"
+        type="button"
+      >
         <div className="py-2 pl-3 pr-5">
           <CiSearch className="size-6" />
         </div>
